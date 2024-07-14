cad43908f898e278b3fd24a2541053f02d8096f8
yfukagawa <yfukagawa@rooseveltinnovations.com>
2024-07-14 12:48:29 -0400

    adding spring-mongo servicd

---
+++ procedure-spring-mongo-service/src/main/java/com/java/procedure_spring_mongo_service/ProcedureSpringMongoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.java.procedure_spring_mongo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcedureSpringMongoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcedureSpringMongoServiceApplication.class, args);
+	}
+
+}
---
+++ procedure-spring-mongo-service/src/test/java/com/java/procedure_spring_mongo_service/ProcedureSpringMongoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.procedure_spring_mongo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcedureSpringMongoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
